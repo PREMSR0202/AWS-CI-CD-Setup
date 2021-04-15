682db2a4e9d485af83a3839406f19bed1c204cb7
Prem S R <64535689+PREMSR0202@users.noreply.github.com>
2021-04-15 12:16:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.get('/',(req,res)=>{
 })
 
 function random(req,res){
-    var length = 7;
+    var length = 5;
     var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters       = '0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
